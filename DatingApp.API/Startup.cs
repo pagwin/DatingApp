269f04c73c8a28e9aa426150ea0878df7eac8d57
Paul Goodwin <paulsposttray@pgemail.com>
2020-07-28 14:12:58 +0100

    Pre-publishing

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -33,10 +33,25 @@ namespace DatingApp.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite
                 (Configuration.GetConnectionString("DefaultConnection")));
+
+            ConfigureServices(services);
+        }
+
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => x.UseMySql
+                (Configuration.GetConnectionString("DefaultConnection")));
+
+            ConfigureServices(services);
+        }        
+        public void ConfigureServices(IServiceCollection services)
+        {
+
             services.AddControllers().AddNewtonsoftJson();
             services.AddControllers().AddNewtonsoftJson(opt =>
             {
@@ -92,9 +107,13 @@ namespace DatingApp.API
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
